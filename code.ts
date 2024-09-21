406d9cb8976a3148f3f1e4de89d5b59dd90a9422
Jonathan PD <jonathan@produzdigital.com>
2024-09-21 13:22:53 -0300

    fix: if Invalid count value

--- code.ts
+++ code.ts
@@ -3,11 +3,15 @@ figma.showUI(__html__);
 figma.ui.onmessage = async (msg: { type: string, count?: number }) => {
   if (msg.type === 'create-rectangles') {
     const nodes: SceneNode[] = [];
+    if (typeof msg.count !== 'number') {
+      console.error('Invalid count value');
+      return;
+    }
+
     for (let i = 0; i < msg.count; i++) {
       // createRectangle()
       const rect = figma.createRectangle();
       rect.x = i * 150;
-
       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
