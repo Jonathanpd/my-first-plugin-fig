63c3afd551de2dc757882d3fe90fbaf7018652df
Jonathan PD <jonathan@produzdigital.com>
2024-09-21 19:09:17 -0300

    feat: console values | 2 | in code.ts

--- code.ts
+++ code.ts
@@ -1,3 +1,8 @@
 figma.showUI(__html__);
 
 figma.ui.resize(600,500);
+
+figma.ui.onmessage = pluginMessage => {
+    const {name, username, description, darkModeState, imageVariant} = pluginMessage;
+    console.log(name, username, description, darkModeState, imageVariant);
+}
