ab5111449f7a45d2cec8f13c6250956c06a6bc56
Jonathan PD <jonathan@produzdigital.com>
2024-09-21 21:21:27 -0300

    feat: show errors (try catch) | likes and comments random | viewport.scrollAndZoomIntoView

--- code.ts
+++ code.ts
@@ -8,6 +8,8 @@ async function init() {
     figma.ui.onmessage = async (pluginMessage) => {
         await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
 
+        const nodes:SceneNode[] = [];
+
         const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
         let selectedVariant;
 
@@ -40,18 +42,33 @@ async function init() {
         //const variants = postComponentSet.findAll(node => node.type === "COMPONENT");
         //console.log(variants.map(v => v.name)); 
 
-        if (selectedVariant) {
+        try {
             const newPost = selectedVariant.createInstance();
+            if (!newPost) {
+                console.error("Falha ao criar uma nova instância.");
+            }
 
             const templateName = newPost.findOne(node => node.name == "displayName" && node.type == "TEXT") as TextNode;
             const templateUsername = newPost.findOne(node => node.name == "@username" && node.type == "TEXT") as TextNode;
             const templateDescription = newPost.findOne(node => node.name == "description" && node.type == "TEXT") as TextNode;
+            const numLikes = newPost.findOne(node => node.name == "likesLabel" && node.type == "TEXT") as TextNode;
+            const numComments = newPost.findOne(node => node.name == "commentsLabel" && node.type == "TEXT") as TextNode;
 
             templateName.characters = pluginMessage.name;
             templateUsername.characters = pluginMessage.username;
             templateDescription.characters = pluginMessage.description;
-        } else {
-            console.error("Nenhuma variante selecionada encontrada.");
+            numLikes.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+            numComments.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+
+            if (newPost) {
+                nodes.push(newPost);
+            } else {
+                console.error("newPost não é válido.");
+            }
+
+            figma.viewport.scrollAndZoomIntoView(nodes);
+        } catch (error) {
+            console.error("Erro ao criar instância:", error);
         }
 
         figma.closePlugin();
